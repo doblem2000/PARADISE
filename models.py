f4d96df37e73c0ae12c35e7c201c62b88e4932ae
Poggi28 <paolinosasa@gmail.com>
2023-07-16 12:17:36 +0200

    Add build_ResNet50

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import torch
+from torchvision.models import resnet50, ResNet50_Weights
 
 def build_MobileNetV3Small(num_outputs=1):
   model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v3_small', pretrained=True)
@@ -17,4 +18,11 @@ def build_MobileNetV2(num_outputs=1):
   model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
   model.classifier = torch.nn.Sequential(torch.nn.Dropout(p=0.2, inplace=False),
                                          torch.nn.Linear(in_features=1280, out_features=num_outputs, bias=True))
+  return model
+
+def build_ResNet50(num_outputs=1):
+  model = resnet50(ResNet50_Weights.DEFAULT) # .DEFAULT = best available weights
+  model.fc=torch.nn.Linear(in_features=2048,
+                    out_features=num_outputs, # same number of output units as our number of classes
+                    bias=True)
   return model
