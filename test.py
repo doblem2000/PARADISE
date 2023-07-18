e7680b34976d99e287dd3ac7a22ee6016c2902ae
my-rice <dvdrsi2017@gmail.com>
2023-07-18 22:52:31 +0200

    ...

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
-from PIL import Image
-import cv2, os, argparse, random
-import numpy as np
+import cv2, os, argparse
 import torch
-from torch.nn import Linear,Sequential,Dropout
 import albumentations
-from VideoFrameDataset import ImglistOrdictToTensor
 from torchvision import transforms
 from models import *
-import time
-from firenet import FireNet,build_FireNet
+#import time
+from firenet import *
 from firenetV2 import FireNetV2
 
 def init_parameter():   
@@ -17,168 +13,144 @@ def init_parameter():
     parser.add_argument("--results", type=str, default='foo_results/', help="Results folder")
     args = parser.parse_args()
     return args
-
-
-### TODO: CODICE AGGIUNTO
-def classify_video(predictions, threshold=0.5, min_duration=10):
-  video_length = len(predictions)
-  start_frame = None
-  fire_duration = 0
-
-  for frame_idx in range(video_length):
-    if predictions[frame_idx] >= threshold:
-      if start_frame is None:
-        start_frame = frame_idx
-      fire_duration += 1
-    else:
-      if fire_duration >= min_duration:
-        return 1, start_frame  
-
-      start_frame = None
-      fire_duration = 0
-
-  if fire_duration >= min_duration:
-    return 1, start_frame  
-  else:
-    return 0, None  
-################################################
-
 args = init_parameter()
 
-### TODO: CODICE AGGIUNTO 
+
 # Here you should initialize your method
-WEIGHT_PATH = 'ResNet50_exp10_2000epoch_5fold_5segment_1frampersegment_batchsize32/fold_0_best_model.pth'
-MIN_DURATION = 7
+WEIGHT_PATH = 'FireNetV2_400epoch_10fold_3segment_1frampersegment_batchsize32/fold_8_best_model.pth'
+MIN_DURATION = 10
 THRESHOLD = 0.5
-total_frames = 0
-total_time = 0
 
-##### CREAZIONE DEL MODELLO #####
-### TODO: caricare il modello scelto da noi !!!!!!!!!!!!!!!!!!!!!
+#####  MODEL CREATION #####
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#model = build_ResNet18(1)
+#model = build_ResNet50(1)
 model = build_FireNetV2()
-model.load_state_dict(torch.load(WEIGHT_PATH))
+#model = build_MobileNetV3Small(1)
+model.load_state_dict(torch.load(WEIGHT_PATH,map_location=device))
 #model = build_FireNet()
 
 model = model.cuda() if torch.cuda.is_available() else model
 model.eval()
-########### FINE CODICE NOSTRO AGGIUNTO
 
-print('Videos folder: {}'.format(args.videos),"Current working directory: ", os.getcwd())
+#print('Videos folder: {}'.format(args.videos),"Current working directory: ", os.getcwd())
 num_videos: int = len(os.listdir(args.videos))
-print('Numero di video: {}'.format(num_videos))
+#print('Numero di video: {}'.format(num_videos))
 
+total_frames: int = 0
+total_time = 0
 num_frames: int = 0
-frames = []
+num_imgs: int = 0
+start_frame_fire = None
+fire_duration: int = 0
+video_prediction = None
 ################################################
 
-
-
-### CODICE PROFESSORE
+# For all the test videos
 for video in os.listdir(args.videos):
-    print(video)
+    # Process the video
     ret = True
     cap = cv2.VideoCapture(os.path.join(args.videos, video))
     fps = round(cap.get(cv2.CAP_PROP_FPS))
     while ret:
         ret, img = cap.read()
-### FINE CODICE PROFESSORE
-
-        ### CODICE AGGIUNTO
         # Here you should add your code for applying your method
+        
+        #start_time = time.time()
         if ret == True:
-          if num_frames % fps == 0: # Prendo un frame ogni secondo
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            
-            ##### PREPROCESSING IMAGES #####
-            #TODO: dai video si estraggono le immagini 
-            transform = albumentations.Compose([
-                albumentations.Resize(height=64, width=64, interpolation=1, always_apply=True),
-                albumentations.Normalize(mean=[0.485, 0.456, 0.406],
-                                        std=[0.229, 0.224, 0.225],
-                                        max_pixel_value=255.,
-                                        always_apply=True),
-            ])
-            img = transform(image=img)["image"]
+            if num_frames % fps == 0: # Prendo un frame ogni secondo
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                
+                ##### PREPROCESSING IMAGES #####
 
-            ########################################################
+                #### Firenet
+                transform = albumentations.Compose([
+                    albumentations.Resize(height=64, width=64, interpolation=1, always_apply=True),
+                    albumentations.Normalize(mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225],
+                                            max_pixel_value=255.,
+                                            always_apply=True),
+                ])
+                # transform = albumentations.Sequential([
+                #     albumentations.Resize(height=224, width=224, interpolation=1, always_apply=True),
+                #     albumentations.Normalize(mean=[0.485, 0.456, 0.406],
+                #                  std=[0.229, 0.224, 0.225],
+                #                  max_pixel_value=255.,
+                #                  always_apply=True),
+                # ])
+                
+                img = transform(image=img)["image"]
+                
+                with torch.no_grad():
+                    
+                    input_batch = transforms.functional.to_tensor(img).unsqueeze(0) # create a mini-batch as expected by the model        
+                    if torch.cuda.is_available():
+                        input_batch = input_batch.to('cuda')
+                        
+                    output = model(input_batch)
+                
+                ###################### 
+                prediction = FireNetV2.compute_output(output[0])
+                #prediction = torch.nn.functional.sigmoid(output[0]) 
+                ######################
+               
+                ####### CLASSIFICATION LOGIC #######
+                if prediction >= THRESHOLD: # Fire detected in the current frame
+                    #print("prediction >= THRESHOLD: ", num_imgs)
+                    if start_frame_fire is None:
+                        start_frame_fire = num_imgs
+                    fire_duration += 1
+                    #print("prediction >= THRESHOLD. fire_duration: ",fire_duration, "start_frame_fire: ", start_frame_fire)
+                else: # No fire detected in the current frame
+                    #print("prediction < THRESHOLD. prediction: ",prediction)
+                    fire_duration = 0
+                    start_frame_fire = None
 
-            frames.append(img)
+                if fire_duration >= MIN_DURATION: # Fire detected for at least MIN_DURATION seconds
+                    #print("Fire detected for at least MIN_DURATION seconds. start_frame_fire: ",start_frame_fire,"num_imgs" ,num_imgs)
+                    img = None
+                    num_imgs += 1
+                    video_prediction = 1
+                    break
+                        
+                num_imgs += 1
+            num_frames += 1
             img = None
-          num_frames += 1
-        else:
-          #
-          frames_old = frames
-          #
-          #frames_tensor = ImglistOrdictToTensor.forward(frames)
-          #frames_tensor = frames_tensor.cuda() if torch.cuda.is_available() else frames_tensor
-          
-          frames = []
-          #print("frames_tensor.size()",frames_tensor.size())
+
+    #end_time = time.time()
+
         ########################################################
-
-
-
-    ### CODICE PROFESSORE
     cap.release()
     f = open(args.results+video+".txt", "w")
-    ### FINE CODICE PROFESSORE 
 
-
-    ### CODICE AGGIUNTO
     # Here you should add your code for writing the results
-    frames_predictions = {}
-    with torch.no_grad():
-      #model.eval()
-      #output = model(frames_tensor)
-      #frame_predictions = torch.nn.functional.sigmoid(output)
-      
+    if video_prediction is None and start_frame_fire == 0 and prediction >= THRESHOLD: 
+        # If the fire video is shorter than MIN_DURATION, 
+        # we consider it as a fire video if: the first frame is classified as fire (start_frame_fire == 0 ) and the last frame is also classified as fire. 
+        # This means that all frames of the video are classified as fire.
+        video_prediction = 1
 
-      model.eval()
-      for (id,frame) in enumerate(frames_old):
-        start_time = time.time()
-        input_batch = transforms.functional.to_tensor(frame).unsqueeze(0) # create a mini-batch as expected by the model        
-
-        if torch.cuda.is_available():
-          input_batch = input_batch.to('cuda')
-          model.to('cuda')
-        
-        output2 = model(input_batch)
-        frames_predictions[id] = FireNetV2.compute_output(output2[0])
-        #frames_predictions[id] = torch.nn.functional.sigmoid(output2) 
-        
-        end_time = time.time()
-    #   for (id,_) in enumerate(frames_old):
-    #     print(frame_predictions[id] ,frames_predictions[id])
-        
-      #print(type(frame_predictions))
-      #print("frame_predictions: ",frame_predictions)
-
-
-    min_duration = MIN_DURATION if len(frames_predictions) >= MIN_DURATION else len(frames_predictions)
-    prediction, start_frame = classify_video(frames_predictions, threshold=THRESHOLD, min_duration=min_duration)
-    #min_duration = MIN_DURATION if frame_predictions.size(0) >= MIN_DURATION else frame_predictions.size(0)
-    #prediction, start_frame = classify_video(frame_predictions, threshold=THRESHOLD, min_duration=min_duration)
-
-    if prediction:
-        t = int(start_frame)
+    if video_prediction:
+        t = int(start_frame_fire)
         f.write(str(t))
 
-    #print(torch.cuda.memory_summary(device=None, abbreviated=False))
-    
-    total_frames += num_frames
-    total_time += end_time-start_time
-    end_time = 0
-    start_time = 0
+    total_frames += num_imgs
+    #total_time += end_time-start_time
+    #end_time = 0
+    #start_time = 0
+
     num_frames = 0
-    frames = []
-    frames_tensor = None
+    num_imgs = 0
+    start_frame_fire = None
+    fire_duration = 0
     prediction = None
+    video_prediction = None
     ########################################################
     f.close()
 
-print("Total frames: ", total_frames)
-print("Total time: ", total_time)
+#print("Total frames: ", total_frames)
+#print("Total time: ", total_time)
 
 
 
---
+++ test_file_old/test.py
@@ -0,0 +1,184 @@
+from PIL import Image
+import cv2, os, argparse, random
+import numpy as np
+import torch
+from torch.nn import Linear,Sequential,Dropout
+import albumentations
+from VideoFrameDataset import ImglistOrdictToTensor
+from torchvision import transforms
+from models import *
+import time
+from firenet import FireNet,build_FireNet
+from firenetV2 import FireNetV2
+
+def init_parameter():   
+    parser = argparse.ArgumentParser(description='Test')
+    parser.add_argument("--videos", type=str, default='foo_videos/', help="Dataset folder")
+    parser.add_argument("--results", type=str, default='foo_results/', help="Results folder")
+    args = parser.parse_args()
+    return args
+
+
+### TODO: CODICE AGGIUNTO
+def classify_video(predictions, threshold=0.5, min_duration=10):
+  video_length = len(predictions)
+  start_frame = None
+  fire_duration = 0
+
+  for frame_idx in range(video_length):
+    if predictions[frame_idx] >= threshold:
+      if start_frame is None:
+        start_frame = frame_idx
+      fire_duration += 1
+    else:
+      if fire_duration >= min_duration:
+        return 1, start_frame  
+
+      start_frame = None
+      fire_duration = 0
+
+  if fire_duration >= min_duration:
+    return 1, start_frame  
+  else:
+    return 0, None  
+################################################
+
+args = init_parameter()
+
+### TODO: CODICE AGGIUNTO 
+# Here you should initialize your method
+WEIGHT_PATH = 'ResNet50_exp10_2000epoch_5fold_5segment_1frampersegment_batchsize32/fold_0_best_model.pth'
+MIN_DURATION = 7
+THRESHOLD = 0.5
+total_frames = 0
+total_time = 0
+
+##### CREAZIONE DEL MODELLO #####
+### TODO: caricare il modello scelto da noi !!!!!!!!!!!!!!!!!!!!!
+
+model = build_FireNetV2()
+model.load_state_dict(torch.load(WEIGHT_PATH))
+#model = build_FireNet()
+
+model = model.cuda() if torch.cuda.is_available() else model
+model.eval()
+########### FINE CODICE NOSTRO AGGIUNTO
+
+print('Videos folder: {}'.format(args.videos),"Current working directory: ", os.getcwd())
+num_videos: int = len(os.listdir(args.videos))
+print('Numero di video: {}'.format(num_videos))
+
+num_frames: int = 0
+frames = []
+################################################
+
+
+
+### CODICE PROFESSORE
+for video in os.listdir(args.videos):
+    print(video)
+    ret = True
+    cap = cv2.VideoCapture(os.path.join(args.videos, video))
+    fps = round(cap.get(cv2.CAP_PROP_FPS))
+    while ret:
+        ret, img = cap.read()
+### FINE CODICE PROFESSORE
+
+        ### CODICE AGGIUNTO
+        # Here you should add your code for applying your method
+        if ret == True:
+          if num_frames % fps == 0: # Prendo un frame ogni secondo
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            
+            ##### PREPROCESSING IMAGES #####
+            #TODO: dai video si estraggono le immagini 
+            transform = albumentations.Compose([
+                albumentations.Resize(height=64, width=64, interpolation=1, always_apply=True),
+                albumentations.Normalize(mean=[0.485, 0.456, 0.406],
+                                        std=[0.229, 0.224, 0.225],
+                                        max_pixel_value=255.,
+                                        always_apply=True),
+            ])
+            img = transform(image=img)["image"]
+
+            ########################################################
+
+            frames.append(img)
+            img = None
+          num_frames += 1
+        else:
+          #
+          frames_old = frames
+          #
+          #frames_tensor = ImglistOrdictToTensor.forward(frames)
+          #frames_tensor = frames_tensor.cuda() if torch.cuda.is_available() else frames_tensor
+          
+          frames = []
+          #print("frames_tensor.size()",frames_tensor.size())
+        ########################################################
+
+
+
+    ### CODICE PROFESSORE
+    cap.release()
+    f = open(args.results+video+".txt", "w")
+    ### FINE CODICE PROFESSORE 
+
+
+    ### CODICE AGGIUNTO
+    # Here you should add your code for writing the results
+    frames_predictions = {}
+    with torch.no_grad():
+      #model.eval()
+      #output = model(frames_tensor)
+      #frame_predictions = torch.nn.functional.sigmoid(output)
+      
+
+      model.eval()
+      for (id,frame) in enumerate(frames_old):
+        start_time = time.time()
+        input_batch = transforms.functional.to_tensor(frame).unsqueeze(0) # create a mini-batch as expected by the model        
+
+        if torch.cuda.is_available():
+          input_batch = input_batch.to('cuda')
+          model.to('cuda')
+        
+        output2 = model(input_batch)
+        frames_predictions[id] = FireNetV2.compute_output(output2[0])
+        #frames_predictions[id] = torch.nn.functional.sigmoid(output2) 
+        
+        end_time = time.time()
+    #   for (id,_) in enumerate(frames_old):
+    #     print(frame_predictions[id] ,frames_predictions[id])
+        
+      #print(type(frame_predictions))
+      #print("frame_predictions: ",frame_predictions)
+
+
+    min_duration = MIN_DURATION if len(frames_predictions) >= MIN_DURATION else len(frames_predictions)
+    prediction, start_frame = classify_video(frames_predictions, threshold=THRESHOLD, min_duration=min_duration)
+    #min_duration = MIN_DURATION if frame_predictions.size(0) >= MIN_DURATION else frame_predictions.size(0)
+    #prediction, start_frame = classify_video(frame_predictions, threshold=THRESHOLD, min_duration=min_duration)
+
+    if prediction:
+        t = int(start_frame)
+        f.write(str(t))
+
+    #print(torch.cuda.memory_summary(device=None, abbreviated=False))
+    
+    total_frames += num_frames
+    total_time += end_time-start_time
+    end_time = 0
+    start_time = 0
+    num_frames = 0
+    frames = []
+    frames_tensor = None
+    prediction = None
+    ########################################################
+    f.close()
+
+print("Total frames: ", total_frames)
+print("Total time: ", total_time)
+
+
+
