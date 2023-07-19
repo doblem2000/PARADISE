423a8cf402d16cb073c095fb269db9149688f0a7
my-rice <dvdrsi2017@gmail.com>
2023-07-19 18:03:27 +0200

    ...

--- trash.py
+++ trash.py
@@ -8,50 +8,6 @@ from collections import defaultdict
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 
-def tabulate_events(dpath):
-    summary_iterators = [EventAccumulator(os.path.join(dpath, dname)).Reload() for dname in os.listdir(dpath)]
-
-    tags = summary_iterators[0].Tags()['scalars']
-
-    for it in summary_iterators:
-        assert it.Tags()['scalars'] == tags
-
-    out = defaultdict(list)
-    steps = []
-
-    for tag in tags:
-        steps = [e.step for e in summary_iterators[0].Scalars(tag)]
-
-        for events in zip(*[acc.Scalars(tag) for acc in summary_iterators]):
-            assert len(set(e.step for e in events)) == 1
-
-            out[tag].append([e.value for e in events])
-
-    return out, steps
-
-
-def to_csv(dpath):
-    dirs = os.listdir(dpath)
-    print("dirs: ",dirs)
-    d, steps = tabulate_events(dpath)
-    print("d: ",d)
-    print("steps: ",steps)
-    tags, values = zip(*d.items())
-    np_values = np.array(values)
-
-    for index, tag in enumerate(tags):
-        df = pd.DataFrame(np_values[index], index=steps, columns=dirs)
-        df.to_csv(get_file_path(dpath, tag))
-
-
-def get_file_path(dpath, tag):
-    file_name = tag.replace("/", "_") + '.csv'
-    folder_path = os.path.join(dpath, 'csv')
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    return os.path.join(folder_path, file_name)
-
-
 def print_data(path):
     event_acc = EventAccumulator(path)
     print(event_acc.Reload())
@@ -66,7 +22,7 @@ def write_csv(dir,fold,headers,data):
         writer.writeheader()
         writer.writerows(data)
 
-def create_csv(path,dir,fields):
+def create_csv(path,dir,fields,type='train/loss'):
     event_acc = EventAccumulator(path)
     print(event_acc.Reload())
     print(event_acc.Tags())
@@ -86,20 +42,15 @@ def create_csv(path,dir,fields):
         train_loss.append({fields[0]: step, fields[1]: value})
     
 
-
-# field names 
-
-
-
-
-
 if __name__ == '__main__':
     path = "FireNetV2_400epoch_10fold_3segment_1frampersegment_batchsize32/ignore/events.out.tfevents.1689673499.f695224c4e89.487.0"
     #print_data(path)
     fields = ['Step', 'Value']
-    dir = "csv/FireNetV2_400epoch_10fold_3segment_1frampersegment_batchsize32/"
-    os.makedirs(dir,exist_ok=True)
-    train_loss = create_csv(path,dir,fields)
+    graph_types = ['train/loss','train/accuracy','val/loss','val/accuracy']
+    for graph_type in graph_types:
+        dir = "csv/FireNetV2_400epoch_10fold_3segment_1frampersegment_batchsize32/" + graph_type + "/"
+        os.makedirs(dir,exist_ok=True)
+        train_loss = create_csv(path,dir,fields,graph_type)
     
     #print(train_loss)
     #print(train_loss.keys(),len(train_loss['fold0']),len(train_loss['fold1']),len(train_loss['fold2']))
